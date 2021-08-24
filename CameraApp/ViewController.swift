9b19aff407d5b450a1a43a006f08f1de280d9e81
将之大西 <omccello@gmail.com>
2021-08-24 23:35:38 +0900

    add valid camera available

--- CameraApp/ViewController.swift
+++ CameraApp/ViewController.swift
@@ -1,22 +1,19 @@
-//
-//  ViewController.swift
-//  CameraApp
-//
-//  Created by 大西将之 on 2021/08/22.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
     @IBOutlet weak var photoImage: UIImageView!
     @IBAction func cameraLaunchAction(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera can be used.")
+        } else {
+            print("Camera is not available.")
+        }
     }
     @IBAction func shareAction(_ sender: Any) {
     }
