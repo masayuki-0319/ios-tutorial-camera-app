92b0ff63566d7743587afd9e7acfd07b60e9ecae
将之大西 <omccello@gmail.com>
2021-08-24 23:48:11 +0900

    add get UIImage

--- CameraApp/ViewController.swift
+++ CameraApp/ViewController.swift
@@ -11,11 +11,25 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBAction func cameraLaunchAction(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             print("Camera can be used.")
+            let ipc = UIImagePickerController()
+            ipc.sourceType = .camera
+            ipc.delegate = self
+            
+            present(ipc, animated: true, completion: nil)
         } else {
             print("Camera is not available.")
         }
     }
     @IBAction func shareAction(_ sender: Any) {
+    }
+    
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : Any]
+    ) {
+        photoImage.image = info["UIImagePickerControllerOriginalImage"] as? UIImage
+        
+        dismiss(animated: true, completion: nil)
     }
 }
 
